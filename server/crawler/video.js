f975bf8d8ab299f85666bff69d136114c5eec582
libin <838300414@qq.com>
2018-07-26 14:33:34 +0800

    子进程爬取豆瓣某电影详情的预告片海报和预告片视频链接

---
+++ server/crawler/video.js
@@ -0,0 +1,70 @@
+
+const puppeteer = require('puppeteer')
+const base = "https://movie.douban.com/subject/"
+const doubanId = "26654498"
+const videoBase = "https://movie.douban.com/trailer/26654498"
+const sleep = time => new Promise(resolve=>{
+  setTimeout(resolve,time)
+})
+
+
+//自动执行的异步函数
+;(async ()=>{
+  console.log('Start visit the target page')
+
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox'],
+    dumpio: false
+  })
+
+  const page = await browser.newPage()
+  await page.goto(base + doubanId,{
+    waitUntil: 'networkidle2'
+  })
+  await sleep(100)
+
+  const result = await page.evaluate(() =>{
+    var $ = window.$
+    var it = $('.related-pic-video')
+    
+
+    if(it && it.length > 0){
+      var link = it.attr('href')
+      var cover = it.attr('style').replace('background-image:url(','').replace('?)','')
+
+      return {
+        link,
+        cover
+      }
+    }
+
+    return {}
+  })
+  //继续爬取
+  let video
+
+  if(result.link){
+    await page.goto(result.link,{
+      waitUntil:'networkidle2'
+    })
+    await sleep(2000)
+
+    video = await page.evaluate(()=>{
+      var $ = window.$
+      var it = $('source')
+      if(it && it.length>0){
+        return it.attr('src')
+      }
+      return ''
+    })
+  }
+
+  const data = {
+    video,
+    doubanId,
+    cover: result.cover
+  }
+  browser.close()
+  process.send(data)
+  process.exit(0)
+})()
--- server/tasks/trailer.js
+++ server/tasks/trailer.js
@@ -2,7 +2,7 @@ const cp = require('child_process')
 const { resolve } = require('path')
 
 ;(async ()=>{
-  const script = resolve(__dirname,'../crawler/trailer-list')
+  const script = resolve(__dirname,'../crawler/video')
   const child = cp.fork(script, [])
   let invoked = false
 
@@ -20,7 +20,7 @@ const { resolve } = require('path')
   })
 
   child.on('message', data => {
-    let result = data.result
+    let result = data
     console.log(result)
   })
 })()
